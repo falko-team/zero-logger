3ed56a6a892a84d22e41cc165a2e6442b48c8975
Rima Falko <rima@falko.email>
2025-04-22 16:19:18 +0200

    Refactor examples

--- Examples/Falko.ZeroLogger.Examples/Program.cs
+++ Examples/Falko.ZeroLogger.Examples/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System.Logging;
 
-LoggerRuntime.Initialize(new LoggerFileTarget(SimpleLogInterpolator.Instance, "falko", "./Logs"));
+var fileTarget = new LoggerFileTarget(SimpleLogInterpolator.Instance, "falko", "./Logs");
+
+LoggerRuntime.Initialize(fileTarget);
 
 var logger = LoggerFactory.CreateLoggerOfType<Program>();
 
 logger.Info(static () => "Hello, {MachineName}", static () => Environment.MachineName);
+logger.Trace(static () => "System is {OSVersion}", static () => Environment.OSVersion);
 
 LoggerRuntime.Dispose();
