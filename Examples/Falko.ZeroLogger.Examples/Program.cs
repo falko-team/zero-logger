9c96fa4997aca174e3f29034503e0494ecddb084
Rima Falko <fembina@pm.me>
2025-04-22 16:45:01 +0200

    Implement LogMessageArgumentsInterpolator

--- Examples/Falko.ZeroLogger.Examples/Program.cs
+++ Examples/Falko.ZeroLogger.Examples/Program.cs
@@ -6,7 +6,7 @@ LoggerRuntime.Initialize(fileTarget);
 
 var logger = LoggerFactory.CreateLoggerOfType<Program>();
 
-logger.Info(static () => "Hello, {MachineName}", static () => Environment.MachineName);
-logger.Trace(static () => "System is {OSVersion}", static () => Environment.OSVersion);
+logger.Info(static () => "Hello, {0}", static () => Environment.UserName);
+logger.Trace(static () => "System is {0}", static () => Environment.OSVersion);
 
 LoggerRuntime.Dispose();
