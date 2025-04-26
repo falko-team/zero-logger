b8fdc6fc0fec94920090fc254c64136ac4e6f1f8
Rima Falko <fembina@pm.me>
2025-04-26 23:46:57 +0200

    Add more checks for LogMessageArgumentsInterpolator and cleanups

--- Sources/Falko.ZeroLogger/Interpolators/LogMessageArgumentsInterpolator.cs
+++ Sources/Falko.ZeroLogger/Interpolators/LogMessageArgumentsInterpolator.cs
@@ -10,6 +10,8 @@ internal static class LogMessageArgumentsInterpolator
     public static string? Interpolate(string? message,
         string? argument)
     {
+        if (message is null) return null;
+
         const int argumentCount = 1;
 
         var arrays = ArrayPool<string?>.Shared;
@@ -21,6 +23,9 @@ internal static class LogMessageArgumentsInterpolator
         Unsafe.Add(ref argumentsRef, 0) = argument;
 
         message = Interpolate(message, ref argumentsRef, argumentCount);
+        message = InterpolateCore(message, ref argumentsRef, argumentCount);
+
+        Unsafe.Add(ref argumentsRef, 0) = null;
 
         arrays.Return(arguments);
 
@@ -32,6 +37,8 @@ internal static class LogMessageArgumentsInterpolator
         string? argument1,
         string? argument2)
     {
+        if (message is null) return null;
+
         const int argumentCount = 2;
 
         var arrays = ArrayPool<string?>.Shared;
@@ -43,7 +50,10 @@ internal static class LogMessageArgumentsInterpolator
         Unsafe.Add(ref argumentsRef, 0) = argument1;
         Unsafe.Add(ref argumentsRef, 1) = argument2;
 
-        message = Interpolate(message, ref argumentsRef, argumentCount);
+        message = InterpolateCore(message, ref argumentsRef, argumentCount);
+
+        Unsafe.Add(ref argumentsRef, 0) = null;
+        Unsafe.Add(ref argumentsRef, 1) = null;
 
         arrays.Return(arguments);
 
@@ -56,6 +66,8 @@ internal static class LogMessageArgumentsInterpolator
         string? argument2,
         string? argument3)
     {
+        if (message is null) return null;
+
         const int argumentCount = 3;
 
         var arrays = ArrayPool<string?>.Shared;
@@ -68,7 +80,11 @@ internal static class LogMessageArgumentsInterpolator
         Unsafe.Add(ref argumentsRef, 1) = argument2;
         Unsafe.Add(ref argumentsRef, 2) = argument3;
 
-        message = Interpolate(message, ref argumentsRef, argumentCount);
+        message = InterpolateCore(message, ref argumentsRef, argumentCount);
+
+        Unsafe.Add(ref argumentsRef, 0) = null;
+        Unsafe.Add(ref argumentsRef, 1) = null;
+        Unsafe.Add(ref argumentsRef, 2) = null;
 
         arrays.Return(arguments);
 
@@ -82,6 +98,8 @@ internal static class LogMessageArgumentsInterpolator
         string? argument3,
         string? argument4)
     {
+        if (message is null) return null;
+
         const int argumentCount = 4;
 
         var arrays = ArrayPool<string?>.Shared;
@@ -95,7 +113,13 @@ internal static class LogMessageArgumentsInterpolator
         Unsafe.Add(ref argumentsRef, 2) = argument3;
         Unsafe.Add(ref argumentsRef, 3) = argument4;
 
-        message = Interpolate(message, ref argumentsRef, argumentCount);
+        message = InterpolateCore(message, ref argumentsRef, argumentCount);
+
+        Unsafe.Add(ref argumentsRef, 0) = null;
+        Unsafe.Add(ref argumentsRef, 1) = null;
+        Unsafe.Add(ref argumentsRef, 2) = null;
+        Unsafe.Add(ref argumentsRef, 3) = null;
+        Unsafe.Add(ref argumentsRef, 4) = null;
 
         arrays.Return(arguments);
 
@@ -104,6 +128,14 @@ internal static class LogMessageArgumentsInterpolator
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string? Interpolate(string? message, ref string? arguments, int count)
+    {
+        if (message is null) return null;
+
+        return InterpolateCore(message, ref arguments, count);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static string? InterpolateCore(string message, ref string? arguments, int count)
     {
         return message;
     }
