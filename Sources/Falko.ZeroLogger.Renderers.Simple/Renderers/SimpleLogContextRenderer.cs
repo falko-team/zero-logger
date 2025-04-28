537120577bba91e61599669e7acc51b475b5a29b
Rima Falko <fembina@pm.me>
2025-04-29 01:07:39 +0200

    Optimize and refactor SimpleLogContextRenderer

--- Sources/Falko.ZeroLogger.Renderers.Simple/Renderers/SimpleLogContextRenderer.cs
+++ Sources/Falko.ZeroLogger.Renderers.Simple/Renderers/SimpleLogContextRenderer.cs
@@ -2,6 +2,7 @@ using System.Logging.Builders;
 using System.Logging.Contexts;
 using System.Logging.Logs;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace System.Logging.Renderers;
 
@@ -9,7 +10,7 @@ public sealed class SimpleLogContextRenderer : ILogContextRenderer
 {
     public static readonly SimpleLogContextRenderer Instance = new();
 
-    private static readonly string[] Levels =
+    private static readonly string[] LevelShortNames =
     [
         "Trc",
         "Dbg",
@@ -19,130 +20,160 @@ public sealed class SimpleLogContextRenderer : ILogContextRenderer
         "Ftl"
     ];
 
+    private static readonly string NewLine = Environment.NewLine;
+    private static readonly int NewLineLength = NewLine.Length;
+
+    private const string UnknownException = "UnknownException";
+
+    private const string ExceptionTypeBlockName = "Exception: ";
+    private static readonly int ExceptionTypeBlockNameLength = ExceptionTypeBlockName.Length;
+
+    private const string ExceptionMessageBlockName = "Message: ";
+    private static readonly int ExceptionMessageBlockNameLength = ExceptionMessageBlockName.Length;
+
+    private static readonly string ExceptionStackTraceBlockName = $"Trace:{NewLine}";
+    private static readonly int ExceptionStackTraceBlockNameLength = ExceptionStackTraceBlockName.Length;
+
+    private const int ExceptionBlockPadding = 2;
+
+    private const int BlockMinimumLength = 3;
+
     private SimpleLogContextRenderer() { }
 
+    [MethodImpl(MethodImplOptions.NoInlining)]
     public string Render(in LogContext logContext)
     {
         var dateText = logContext.Time.DateTime.ToShortTimeString();
-        var dateBlockLength = dateText.Length + 3;
 
         var levelText = FormatLevel(logContext.Level);
-        const int levelBlockLength = 6;
+        const int levelTextBlockLength = 3 + BlockMinimumLength;
 
         var sourceText = logContext.Source;
-        var sourceBlockLength = sourceText.Length + 3;
 
         var messageText = logContext.Message.Render();
 
-        var newLineText = Environment.NewLine;
-        var newLineTextLength = newLineText.Length;
-
-        var messageLength = messageText.Length + dateBlockLength + levelBlockLength + sourceBlockLength + newLineTextLength;
+        var messageLength = messageText.Length
+            + GetHeaderBlockLength(dateText)
+            + levelTextBlockLength
+            + GetHeaderBlockLength(sourceText)
+            + NewLineLength;
 
         var exception = logContext.Exception;
 
         if (exception is null)
         {
-            using scoped var messageBuilder = messageLength > 256
+            scoped var messageBuilder = messageLength > 256
                 ? new ValueStringBuilder(messageLength)
                 : new ValueStringBuilder(stackalloc char[messageLength]);
 
-            messageBuilder.Append('[');
-            messageBuilder.Append(dateText);
-            messageBuilder.Append(']');
-            messageBuilder.Append(' ');
+            try
+            {
+                RenderHeader(ref messageBuilder, dateText, levelText, sourceText, messageText);
 
-            messageBuilder.Append('[');
-            messageBuilder.Append(levelText);
-            messageBuilder.Append(']');
-            messageBuilder.Append(' ');
-
-            messageBuilder.Append('[');
-            messageBuilder.Append(sourceText);
-            messageBuilder.Append(']');
-            messageBuilder.Append(' ');
-
-            messageBuilder.Append(messageText);
-
-            messageBuilder.Append(newLineText);
-
-            return messageBuilder.ToString();
+                return messageBuilder.ToString();
+            }
+            finally
+            {
+                messageBuilder.Dispose();
+            }
         }
         else
         {
-            var exceptionTypeName = exception.GetType().FullName;
-            var exceptionTypeBlockName = "  Exception: ";
-            var exceptionTypeBlockLength = exceptionTypeName is null
-                ? 0
-                : exceptionTypeName.Length + exceptionTypeBlockName.Length + newLineTextLength;
+            var exceptionTypeName = exception.GetType().FullName ?? UnknownException;
 
             var exceptionMessage = exception.Message;
-            var exceptionMessageBlockName = "  Message: ";
-            var exceptionMessageBlockLength = string.IsNullOrEmpty(exceptionMessage)
-                ? 0
-                : exceptionMessage.Length + exceptionMessageBlockName.Length + newLineTextLength;
 
             var exceptionStackTrace = exception.StackTrace;
-            var exceptionStackTraceBlockName = "  Trace:";
-            var exceptionStackTraceBlockLength = exceptionStackTrace is null
-                ? 0
-                : exceptionStackTrace.Length + exceptionStackTraceBlockName.Length + newLineTextLength * 2;
+            var exceptionStackTraceBlockLength = exceptionStackTrace is not null
+                ? GetExceptionBlockLength(ExceptionStackTraceBlockNameLength, exceptionStackTrace)
+                : 0;
 
-            messageLength += exceptionStackTraceBlockLength + exceptionMessageBlockLength + exceptionStackTraceBlockLength;
+            messageLength = messageLength
+                + GetExceptionBlockLength(ExceptionTypeBlockNameLength, exceptionTypeName)
+                + GetExceptionBlockLength(ExceptionMessageBlockNameLength, exceptionMessage)
+                + exceptionStackTraceBlockLength
+                + NewLineLength;
 
-            using scoped var messageBuilder = messageLength > 256
+            scoped var messageBuilder = messageLength > 256
                 ? new ValueStringBuilder(messageLength)
                 : new ValueStringBuilder(stackalloc char[messageLength]);
 
-            messageBuilder.Append('[');
-            messageBuilder.Append(dateText);
-            messageBuilder.Append(']');
-            messageBuilder.Append(' ');
-
-            messageBuilder.Append('[');
-            messageBuilder.Append(levelText);
-            messageBuilder.Append(']');
-            messageBuilder.Append(' ');
-
-            messageBuilder.Append('[');
-            messageBuilder.Append(sourceText);
-            messageBuilder.Append(']');
-            messageBuilder.Append(' ');
-
-            messageBuilder.Append(messageText);
-
-            messageBuilder.Append(newLineText);
-
-            if (exceptionTypeBlockLength is not 0)
+            try
             {
-                messageBuilder.Append(exceptionTypeBlockName);
-                messageBuilder.Append(exceptionTypeName);
-                messageBuilder.Append(newLineText);
-            }
+                RenderHeader(ref messageBuilder, dateText, levelText, sourceText, messageText);
 
-            if (exceptionMessageBlockLength is not 0)
+                RenderExceptionBlock(ref messageBuilder, ExceptionTypeBlockName, exceptionTypeName);
+                RenderExceptionBlock(ref messageBuilder, ExceptionMessageBlockName, exceptionMessage);
+
+                if (exceptionStackTraceBlockLength is not 0)
+                {
+                    RenderExceptionBlock(ref messageBuilder, ExceptionStackTraceBlockName, exceptionStackTrace!);
+                }
+
+                messageBuilder.Append(NewLine);
+
+                return messageBuilder.ToString();
+            }
+            finally
             {
-                messageBuilder.Append(exceptionMessageBlockName);
-                messageBuilder.Append(exceptionMessage);
-                messageBuilder.Append(newLineText);
+                messageBuilder.Dispose();
             }
-
-            if (exceptionStackTraceBlockLength is not 0)
-            {
-                messageBuilder.Append(exceptionStackTraceBlockName);
-                messageBuilder.Append(newLineText);
-                messageBuilder.Append(exceptionStackTrace);
-                messageBuilder.Append(newLineText);
-            }
-
-            return messageBuilder.ToString();
         }
     }
 
-    public static string FormatLevel(LogLevel level)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void RenderHeader(scoped ref ValueStringBuilder messageBuilder,
+        string dateText,
+        string levelText,
+        string sourceText,
+        string messageText)
+    {
+        RenderHeaderBlock(ref messageBuilder, dateText);
+        RenderHeaderBlock(ref messageBuilder, levelText);
+        RenderHeaderBlock(ref messageBuilder, sourceText);
+
+        messageBuilder.Append(messageText);
+
+        messageBuilder.Append(NewLine);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int GetHeaderBlockLength(string blockText)
+    {
+        return blockText.Length + BlockMinimumLength;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void RenderHeaderBlock(scoped ref ValueStringBuilder messageBuilder,
+        string blockText)
+    {
+        messageBuilder.Append('[');
+        messageBuilder.Append(blockText);
+        messageBuilder.Append(']');
+        messageBuilder.Append(' ');
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int GetExceptionBlockLength(int blockNameLength, string blockText)
+    {
+        return ExceptionBlockPadding + blockNameLength + blockText.Length + NewLineLength;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void RenderExceptionBlock(scoped ref ValueStringBuilder messageBuilder,
+        string blockName,
+        string blockText)
+    {
+        messageBuilder.Append(' ', ExceptionBlockPadding);
+        messageBuilder.Append(blockName);
+        messageBuilder.Append(blockText);
+        messageBuilder.Append(NewLine);
+    }
+
+    private static string FormatLevel(LogLevel level)
     {
         var index = BitOperations.TrailingZeroCount((int)level);
 
-        return (uint)index < Levels.Length ? Levels[index] : "???";
+        return LevelShortNames[index];
     }
 }
