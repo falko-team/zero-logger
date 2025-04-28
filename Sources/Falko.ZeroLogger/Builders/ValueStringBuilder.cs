259f16dee9271bf807e6af81c37ea96606f61580
Rima Falko <rima@falko.email>
2025-04-29 00:05:53 +0200

    Refactor ValueStringBuilder to use readonly fields and simplify memory management

--- Sources/Falko.ZeroLogger/Builders/ValueStringBuilder.cs
+++ Sources/Falko.ZeroLogger/Builders/ValueStringBuilder.cs
@@ -5,50 +5,23 @@ namespace System.Logging.Builders;
 
 public ref struct ValueStringBuilder : IDisposable
 {
-    private char[]? _debt;
+    private readonly char[]? _debt;
 
-    private Span<char> _buffer;
+    private readonly Span<char> _buffer;
 
     private int _position;
-
-    private int _capacity;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public ValueStringBuilder(Span<char> buffer)
     {
         _buffer = buffer;
-        _capacity = buffer.Length;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public ValueStringBuilder(int capacity)
     {
         _debt = ArrayPool<char>.Shared.Rent(capacity);
-        _buffer = _debt.AsSpan(0, capacity);
-        _capacity = capacity;
-    }
-
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Grow(int capacity)
-    {
-        if ((uint)capacity > (uint)_capacity)
-        {
-            var array = ArrayPool<char>.Shared.Rent(capacity);
-
-            var arraySpan = array.AsSpan(0, capacity);
-
-            _buffer[.._position].CopyTo(arraySpan);
-
-            if (_debt is not null)
-            {
-                ArrayPool<char>.Shared.Return(_debt);
-            }
-
-            _debt = array;
-
-            _buffer = arraySpan;
-            _capacity = capacity;
-        }
+        _buffer = _debt.AsSpan(0);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -84,13 +57,7 @@ public ref struct ValueStringBuilder : IDisposable
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public string Build()
-    {
-        return new string(_buffer[.._position]);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public override string ToString() => Build();
+    public override string ToString() => new(_buffer[.._position]);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Dispose()
@@ -98,6 +65,5 @@ public ref struct ValueStringBuilder : IDisposable
         if (_debt is null) return;
 
         ArrayPool<char>.Shared.Return(_debt);
-        _debt = null;
     }
 }
