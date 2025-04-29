123c2e2b585c528b505238f0b444c3973ed52087
Rima Falko <rima@falko.email>
2025-04-29 17:08:21 +0200

    Optimize DateTimeOffsetProvider and do DateTimeOffsetProvider time more actual

--- Sources/Falko.ZeroLogger/Utils/DateTimeOffsetProvider.cs
+++ Sources/Falko.ZeroLogger/Utils/DateTimeOffsetProvider.cs
@@ -5,30 +5,32 @@ namespace System.Logging.Utils;
 
 public static class DateTimeOffsetProvider
 {
+    private const long CachedTimeUpdateIntervalTicks = 3 * 1000 * 1000; // 3ms
+
     private static readonly Stopwatch Stopwatch = Stopwatch.StartNew();
+
+    private static readonly float StopwatchTickToDateTimeTicks = TimeSpan.TicksPerSecond / (float)Stopwatch.Frequency;
 
     private static DateTimeOffset _cachedTime = DateTimeOffset.Now;
 
-    private static long _lastUpdateTicksTime = Stopwatch.ElapsedTicks;
-
-    private const long CachedTimeUpdateIntervalTicks = 10 * 1000 * 1000; // 10ms
+    private static long _lastTicksTime = Stopwatch.ElapsedTicks;
 
     public static DateTimeOffset Now
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
-            var currentTimestamp = Stopwatch.ElapsedTicks;
+            var currentTicks = Stopwatch.ElapsedTicks;
 
-            var currentTimestampDelta = currentTimestamp - _lastUpdateTicksTime;
+            var currentTicksDelta = currentTicks - _lastTicksTime;
 
-            if (currentTimestampDelta < CachedTimeUpdateIntervalTicks)
+            if (currentTicksDelta < CachedTimeUpdateIntervalTicks)
             {
-                return _cachedTime.AddTicks(currentTimestampDelta);
+                return _cachedTime.AddTicks((long)(currentTicksDelta * StopwatchTickToDateTimeTicks));
             }
 
             _cachedTime = DateTimeOffset.Now;
-            _lastUpdateTicksTime = currentTimestamp;
+            _lastTicksTime = currentTicks;
 
             return _cachedTime;
         }
