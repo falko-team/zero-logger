1412a500fccb45772cb171446b2599b5fa7a0204
Rima Falko <rima@falko.email>
2025-04-29 05:48:07 +0200

    Add benchmarks for ZeroLogger and NLog logging performance

---
+++ Benchmarks/Falko.ZeroLogger.Benchmarks/Program.cs
@@ -0,0 +1,182 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<LogIgnoringBenchmark>();
+BenchmarkRunner.Run<LogRenderingBenchmark>();
+BenchmarkRunner.Run<LogRenderingDosBenchmark>();
+
+public class LogIgnoringBenchmark
+{
+    private static readonly System.Logging.Loggers.Logger ZeroLogger = System.Logging.Factories.LoggerFactory
+        .CreateLoggerOfType<LogIgnoringBenchmark>();
+
+    private static readonly NLog.Logger NLogLogger = NLog.LogManager
+        .GetCurrentClassLogger();
+
+    private const int Iterations = 100;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        LoggerConfigurer.Configure();
+    }
+
+    [Benchmark(Baseline = true)]
+    public void IgnoreZeroLoggerLog()
+    {
+        for (var iteration = 0; iteration < Iterations; iteration++)
+        {
+            ZeroLogger.Trace(static () => "Iteration {IterationNumber}", iteration);
+        }
+    }
+
+    [Benchmark]
+    public void IgnoreNLogLoggerLog()
+    {
+        for (var iteration = 0; iteration < Iterations; iteration++)
+        {
+            NLogLogger.Trace("Iteration {IterationNumber}", iteration);
+        }
+    }
+}
+
+[MemoryDiagnoser]
+public class LogRenderingBenchmark
+{
+    private static readonly System.Logging.Loggers.Logger ZeroLogger = System.Logging.Factories.LoggerFactory
+        .CreateLoggerOfType<LogRenderingBenchmark>();
+
+    private static readonly NLog.Logger NLogLogger = NLog.LogManager
+        .GetCurrentClassLogger();
+
+    private const int Iterations = 100;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        LoggerConfigurer.Configure();
+    }
+
+    [Benchmark(Baseline = true)]
+    public void RenderZeroLoggerLog()
+    {
+        for (var iteration = 0; iteration < Iterations; iteration++)
+        {
+            ZeroLogger.Info("Iteration {IterationNumber}", iteration);
+        }
+    }
+
+    [Benchmark]
+    public void RenderNLogLoggerLog()
+    {
+        for (var iteration = 0; iteration < Iterations; iteration++)
+        {
+            NLogLogger.Info("Iteration {IterationNumber}", iteration);
+        }
+    }
+}
+
+[MemoryDiagnoser]
+public class LogRenderingDosBenchmark
+{
+    private static readonly System.Logging.Loggers.Logger ZeroLogger = System.Logging.Factories.LoggerFactory
+        .CreateLoggerOfType<LogRenderingDosBenchmark>();
+
+    private static readonly NLog.Logger NLogLogger = NLog.LogManager
+        .GetCurrentClassLogger();
+
+    private const int Iterations = 100;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        LoggerConfigurer.Configure();
+    }
+
+    [Benchmark(Baseline = true)]
+    public void RenderZeroLoggerLog()
+    {
+        for (var iteration = 0; iteration < Iterations; iteration++)
+        {
+            ZeroLogger.Info("Iteration {IterationNumber1} {IterationNumber2}", iteration, iteration);
+        }
+    }
+
+    [Benchmark]
+    public void RenderNLogLoggerLog()
+    {
+        for (var iteration = 0; iteration < Iterations; iteration++)
+        {
+            NLogLogger.Info("Iteration {IterationNumber1} {IterationNumber2}", iteration, iteration);
+        }
+    }
+}
+
+file static class LoggerConfigurer
+{
+    public static void Configure()
+    {
+        ConfigureZeroLogger();
+        ConfigureNLogLogger();
+    }
+
+    private static void ConfigureZeroLogger()
+    {
+        System.Logging.Runtimes.LoggerRuntime
+            .Initialize(new System.Logging.Builders.LoggerContextBuilder()
+                .SetLevel(System.Logging.Logs.LogLevels.InfoAndAbove)
+                .AddTarget(System.Logging.Renderers.SimpleLogContextRenderer.Instance,
+                    EmptyZeroLoggerTarget.Instance)
+                .AddTarget(System.Logging.Renderers.SimpleLogContextRenderer.Instance,
+                    EmptyZeroLoggerTarget.Instance)
+                .AddTarget(System.Logging.Renderers.SimpleLogContextRenderer.Instance,
+                    EmptyZeroLoggerTarget.Instance));
+    }
+
+    private static void ConfigureNLogLogger()
+    {
+        var configuration = new NLog.Config.LoggingConfiguration();
+
+        configuration.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal,
+            EmptyNLogLoggerTarget.Instance);
+        configuration.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal,
+            EmptyNLogLoggerTarget.Instance);
+        configuration.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal,
+            EmptyNLogLoggerTarget.Instance);
+
+        NLog.LogManager.Configuration = configuration;
+    }
+}
+
+file sealed class EmptyZeroLoggerTarget : System.Logging.Targets.LoggerTarget
+{
+    public static readonly EmptyZeroLoggerTarget Instance = new();
+
+    private EmptyZeroLoggerTarget() { }
+
+    public override void Initialize(CancellationToken cancellationToken) { }
+
+    public override void Publish(in System.Logging.Contexts.LogContext context,
+        System.Logging.Renderers.ILogContextRenderer renderer,
+        CancellationToken cancellationToken)
+    {
+        _ = renderer.Render(context);
+    }
+
+    public override void Dispose(CancellationToken cancellationToken) { }
+}
+
+file sealed class EmptyNLogLoggerTarget : NLog.Targets.TargetWithLayout
+{
+    public static readonly EmptyNLogLoggerTarget Instance = new()
+    {
+        Layout = "[${time}] [${level:uppercase=true}] [${logger}] ${message}",
+    };
+
+    private EmptyNLogLoggerTarget() { }
+
+    protected override void Write(NLog.LogEventInfo logEvent)
+    {
+        _ = Layout.Render(logEvent);
+    }
+}
